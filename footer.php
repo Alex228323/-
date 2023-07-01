7de692c772cd8014c89630af80a09950770423c8
Alex228323 <118746533+Alex228323@users.noreply.github.com>
2023-07-01 22:34:47 +0500

    Подвал

---
+++ footer.php
@@ -0,0 +1,31 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h3>О нас</h3>
+        <p class="text-justify">Чайный дом - это компания, которая создаёт для вас настоящие произведения исскуства в виде: чайнико, чайных наборов, чашек. Ни один товар не оставит вас равнодушным.</p>
+      </div>
+
+      <div class="col">
+        <h3>Категории</h3>
+        <ul class="footer-links">
+          <li><A href="teapots.php">Чайники</A></li>
+          <li><A href="cups.php">Чашки</A></li>
+          <li><A href="teaset.php">Чайный сервиз</A></li>
+        </ul>
+        <h3>Поделиться</h3>
+        <script src="https://yastatic.net/share2/share.js" async></script>
+        <div class="ya-share2" data-curtain data-size="l" data-shape="round" data-color-scheme="blackwhite" data-services="vkontakte,telegram,viber"></div>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col">
+        <p class="copyright-text">Авторское право &copy; 2022 Все права защищены
+          <a href="#">Чайный дом</a>.
+        </p>
+        <p>Учебный проект</p>
+      </div>
+    </div>
+  </div>
+</footer>
